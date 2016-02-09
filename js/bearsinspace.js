a5ee7d8e31c5fe97bb4702a3dd9387decc1415d0
Alex Johnson <alexmj212@gmail.com>
2016-02-09 10:40:41 -0500

    js move around

---
+++ js/bearsinspace.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+        $('#bg').blurjs({
+                    source: 'body',
+                radius: 10,
+                overlay: 'rgba(0, 0, 0, .2)'
+                });
+});
+
